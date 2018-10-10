363023523c68967a4196daa29e7dfb355eaf1665
Roy Lin <rlin2k1@gmail.com>
2018-10-10 12:10:49 -0700

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {businesses:[]};
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(listOfBusinesses => {this.setState({businesses:listOfBusinesses})});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar searchYelp = {this.searchYelp}/>
+        <BusinessList businesses = {this.state.businesses}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'lt7V8ABW-NNZ0zs5CaIT6oXt81NdWj-aGXopvz_-peeZgGvPYLgwCNPQ8SmYKT8B4W8YiZrpHi-FuHXlnnVIquEfO3YNgY9aFXrEfDxzHBedp3m9Zzw8raS0JQG6WnYx';
+
+const Yelp = {
+	search(term, location, sortBy) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+			{
+				headers:{Authorization:`Bearer ${apiKey}`}
+			}
+			).then(response => { return response.json(); }
+			).then(jsonResponse => {
+				if(jsonResponse.businesses) {
+					return jsonResponse.businesses.map(business => {
+						return {
+							id:business.id,
+							imageSrc:business.image_url,
+							name:business.name,
+							address:business.location.address1,
+							city:business.location.city,
+							state:business.location.state,
+							zipCode:business.location.zip_code,
+							category:business.categories[0].title,
+							rating:business.rating,
+							reviewCount:business.review_count
+						} } );
+				}
+			});
+	}
+};
+
+export default Yelp;
