32c3e0ce10cb033259c35b678e9e0e1d3425e925
Roy Lin <rlin2k1@gmail.com>
2019-01-05 23:38:24 -0800

    Update Yelp.js - Remove API Key

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,4 @@
-const apiKey = 'TaxbA9fhND2ft2Uoa65VCa3Xm2SxRjQMKqG2Hiq_2pmg2xaOFRNWSKm5RrA3AfkNq9vEEhJLSiS5sugb4L_kDN4UcXnqRUAjRenEqlyWAi6kDqzyvJH48spcK1D-W3Yx'
+const apiKey = ''
 const Yelp = {
 	search(term, location, sortBy) {
 		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
